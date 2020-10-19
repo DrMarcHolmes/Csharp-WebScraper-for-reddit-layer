c1d0646c2695e7b3db86920c32c62cf313edf93e
Marc Holmes <marc.holmes@swansea.ac.uk>
2020-10-19 23:10:52 +0100

    Add project files.

---
+++ README.md
@@ -0,0 +1,21 @@
+#Web Scrapping 
+https://dev.to/rachelsoderberg/create-a-simple-web-scraper-in-c-1l1m
+https://stackoverflow.com/questions/307688/how-to-download-a-file-from-a-url-in-c
+https://stackoverflow.com/questions/44074121/build-net-core-console-application-to-output-an-exe
+https://stackoverflow.com/questions/12889269/how-to-create-exe-of-a-console-application
+https://www.windowscentral.com/how-create-automated-task-using-task-scheduler-windows-103
+https://docs.microsoft.com/en-us/dotnet/api/system.datetime.now?view=netcore-3.1
+https://stackoverflow.com/questions/12500091/datetime-tostring-format-that-can-be-used-in-a-filename-or-extension
+
+#Files to be webscraped (gotten from chrome via debug mode)
+https://i.redd.it/layer/t5_32b7p_HOT_final.png
+https://i.redd.it/layer/t5_32b7p_Test%20Canvas,%20Please%20Ignore_HOT_final.png
+https://i.redd.it/layer/t5_32b7p_Me%20IRL_HOT_final.png
+https://i.redd.it/layer/t5_32b7p_Aww_HOT_final.png
+https://i.redd.it/layer/t5_32b7p_Comics_HOT_final.png
+https://i.redd.it/layer/t5_32b7p_Gaming_HOT_final.png
+https://i.redd.it/layer/t5_32b7p_Music_HOT_final.png
+https://i.redd.it/layer/t5_32b7p_NomNom_HOT_final.png
+https://i.redd.it/layer/t5_32b7p_Threads_HOT_final.png
+https://i.redd.it/layer/t5_32b7p_Moviedetails_HOT_final.png
+https://i.redd.it/layer/t5_32b7p_Vaporwaveaesthetics_HOT_final.png
---
+++ WebScraper/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+
+
+namespace WebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets get images!");
+            using (var client = new WebClient())
+            {
+                DateTime localDate = DateTime.Now;
+
+                client.DownloadFile("https://i.redd.it/layer/t5_32b7p_HOT_final.png",                                   "All Layers HOT "+ localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+                Console.WriteLine("All Layers HOT " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+
+                client.DownloadFile("https://i.redd.it/layer/t5_32b7p_Test%20Canvas,%20Please%20Ignore_HOT_final.png",  "Test Canvas, Please Ignore HOT " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+                Console.WriteLine("Test Canvas, Please Ignore HOT " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+
+                client.DownloadFile("https://i.redd.it/layer/t5_32b7p_Me%20IRL_HOT_final.png",                          "Me IRL Hot " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+                Console.WriteLine("Me IRL Hot " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+
+                client.DownloadFile("https://i.redd.it/layer/t5_32b7p_Aww_HOT_final.png",                               "Aww Hot " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+                Console.WriteLine("Aww Hot " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+
+                client.DownloadFile("https://i.redd.it/layer/t5_32b7p_Comics_HOT_final.png",                            "Comics Hot " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+                Console.WriteLine("Comics Hot " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+
+                client.DownloadFile("https://i.redd.it/layer/t5_32b7p_Gaming_HOT_final.png",                            "Gaming Hot " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+                Console.WriteLine("Gaming Hot " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+
+                client.DownloadFile("https://i.redd.it/layer/t5_32b7p_Music_HOT_final.png",                             "Music Hot " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+                Console.WriteLine("Music Hot " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+
+                client.DownloadFile("https://i.redd.it/layer/t5_32b7p_NomNom_HOT_final.png",                            "NonNom Hot " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+                Console.WriteLine("NonNom Hot " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+
+                client.DownloadFile("https://i.redd.it/layer/t5_32b7p_Threads_HOT_final.png",                           "Threads Hot " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+                Console.WriteLine("Threads Hot " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+
+                client.DownloadFile("https://i.redd.it/layer/t5_32b7p_Moviedetails_HOT_final.png",                      "Moviedetail Hot " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+                Console.WriteLine("Moviedetail Hot " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+
+                client.DownloadFile("https://i.redd.it/layer/t5_32b7p_Vaporwaveaesthetics_HOT_final.png",               "Vaporwave Aesthetics Hot " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+                Console.WriteLine("Vaporwave Aesthetics Hot " + localDate.ToString("yyyy-dd-M--HH-mm-ss") + ".png");
+
+            }
+        }
+
+    }
+}
